6f03d107af7f03426f495969f5ac90f7b9ac8972
Yining Shi <ys2189@nyu.edu>
2017-12-31 15:54:46 -0500

    added interactivity

--- sketch.js
+++ sketch.js
@@ -1,19 +1,47 @@
-let step = 0;
-let t;
 let graphs = [];
-let m = 3, n = 3;
+const m = 3, n = 3;
+let speedBtn, resetBtn, colorBtn, aboutText, speedSlider;
+let colorful = true;
 
 function setup() { 
-  createCanvas(windowWidth, windowHeight - 80);
+  createCanvas(windowWidth * 3 / 4, windowHeight);
   background(255);
   colorMode(HSB, 100);
   noFill();
+  initGraphs();
+  speedBtn = createP('SPEED');
+  resetBtn = createP('RESET');
+  colorBtn = createP('COLOR');
+  resetBtn.style('position', windowWidth - windowWidth / 8, 50);
+  colorBtn.style('position', windowWidth - windowWidth / 8, 100);
+  speedBtn.style('position', windowWidth - windowWidth / 8, 150);
+  resetBtn.mouseClicked(reset);
+  colorBtn.mouseClicked(toggleColor);
+  aboutText = select('.about-text');
+  aboutText.style('position', windowWidth / 8, windowHeight);
+  speedSlider = createSlider(1, 20, 8);
+  speedSlider.style('position', windowWidth - windowWidth / 8, 200);
+  // speedSlider.style('width', '20px');
+}
+
+function initGraphs() {
   for (let j = 0; j < n; j++) {
     for (let i = 0; i < m; i++) {
-      graphs.push(new Graph(120 + width * i / m, 120 + height * j / n));
+      graphs.push(new Graph(windowWidth / 8 + width * i / m, 120 + height * j / n));
     }
   }
-} 
+}
+
+function reset() {
+  background(255);
+  graphs = [];
+  initGraphs();
+}
+
+function toggleColor() {
+  colorful = !colorful;
+  reset();
+}
 
 function draw() {
   graphs.forEach(g => {
@@ -21,31 +49,34 @@ function draw() {
   });
 }
 
-function Graph(centerX, centerY) {
-  this.centerX = centerX;
-  this.centerY = centerY;
-  this.ang = 0;
-  this.R = Math.floor(random(90, 120));
-  this.r = Math.floor(random(20, this.R - 20));
-  this.p = Math.floor(random(10, this.r - 10));
-  this.k = this.r / this.R;
-  this.l = this.p / this.r;
+class Graph {
+  constructor(centerX, centerY) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    this.step = 0;
+    this.R = Math.floor(random(90, 120));
+    this.r = Math.floor(random(20, this.R - 20));
+    this.p = Math.floor(random(10, this.r - 10));
+  }
 
-  this.display = function() {
-    if (step < 30000) {  
+  display() {
+    if (this.step < 40000) {
       push();
       translate(this.centerX, this.centerY);
-      t = radians(step);
-      this.ang = ((this.l - this.k) / this.k) * t;
+      let t = radians(this.step);
+      let k = this.r / this.R;
+      let l = this.p / this.r;
+      let ang = ((l - k) / k) * t;
   
-      this.x = this.R * ((1 - this.k) * Math.cos(t) + ((this.l * this.k) * Math.cos(this.ang)));
-      this.y = this.R * ((1 - this.k) * Math.sin(t) - ((this.l * this.k) * Math.sin(this.ang)));
+      this.x = this.R * ((1 - k) * Math.cos(t) + ((l * k) * Math.cos(ang)));
+      this.y = this.R * ((1 - k) * Math.sin(t) - ((l * k) * Math.sin(ang)));
       
-      let h = Math.floor((((step / 150 + this.centerX))) % 100);
-      stroke(h, 60, 80);
-      line(this.prevX, this.prevY, this.x, this.y,);
+      let h = Math.floor((((this.step / 150 + this.centerX))) % 100);
+      if (colorful) stroke(h, 60, 80);
+      else stroke(30);
+      line(this.prevX, this.prevY, this.x, this.y);
       pop();
-      step += 1;
+      this.step += speedSlider.value();
       this.prevX = this.x;
       this.prevY = this.y;
     }
